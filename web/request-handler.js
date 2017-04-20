c0fb3731afcb0970566dce31d9b59c0083a09d92
Hack Reactor Student <student@hackreactor.com>
2017-04-20 14:52:17 -0700

    COMPLETED ALL TEST

--- web/request-handler.js
+++ web/request-handler.js
@@ -8,28 +8,29 @@ exports.handleRequest = function (req, res) {
 
   if (req.method === 'GET') {
 
-    httpHelpers.collectData(req, function(data) {
-      // if data.length
-      res.writeHead(200, httpHelpers.headers);
-      if (req.url !== '/') {
-        // return content
-        console.log('data: ', data);
-        // fs.readFile(archive.paths.archivedSites + data.slice(4))
+    // if data.length
+    res.writeHead(200, httpHelpers.headers);
+    if (req.url !== '/') {
+      // return content
+      fs.readFile(archive.paths.archivedSites + req.url, 'utf8', function(err, data) {
+        if (data) {
+          res.end(data);
+        } else {
+          res.writeHead(404, httpHelpers.headers);
+          res.end();
+        }
+      }); 
+    } else {
+      fs.readFile(path.join(__dirname, '/public/index.html'), 'utf-8', function(err, data) {
 
-        res.end('/google/');
-      } else {
-        fs.readFile(path.join(__dirname, '/public/index.html'), 'utf-8', function(err, data) {
-
-          if (err) {
-            res.writeHead(500);
-            res.end('Server Error!', err);
-            return;
-          }
-          res.end(data); 
-        });
-      } 
-
-    });
+        if (err) {
+          res.writeHead(500);
+          res.end('Server Error!', err);
+          return;
+        }
+        res.end(data); 
+      });
+    }
   }
   if (req.method === 'POST') {
 
